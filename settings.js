80f4d09356487ef085d6f2114fd184617e611bac
dheataa <rdpgratis40@gmail.com>
2022-07-23 21:43:10 +0700

    teuing

---
+++ settings.js
@@ -0,0 +1,79 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const  { indonesia, english, spanyol} = require(`./language`)
+
+// Website Api
+global.APIs = {
+	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan ke owner
+}
+
+// Free apikey
+global.APIKeys = {
+	'https://api.zeeoneofc.xyz': 'V12',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+}
+
+// setting 
+global.autoread = false // auto read pesan / message
+global.autobio = true 
+
+//language
+//Available in indonesia , english & spanyol 
+//You can request others language 
+global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
+
+
+// Other
+// Other
+global.botname = "Sky4You-MD" //namabot kalian
+global.ownername= "SkyllerOFC" //nama kalian
+global.myweb ="https://instagram.com/sky4you" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/sky is good" //bebas asal jan hapus
+global.github = "https://github.io/dheataa" //bebas
+global.email = "skyllerofc@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.timezone = 'Asia/Jakarta' //  timezone wib
+global.premium = ['79615696897'] //premium user
+global.owner = ["79615696897","6285817627091"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "79615696897" // nomor wa kalian
+global.ownernomerr = "+79615696897" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.packname = '© Sky4You-MD' //sticker wm ubah
+global.author = 'Di Buat Oleh SkyllerOFC' //sticker wm ganti nama kalian
+global.sessionName = 'session'
+global.typemenu = 'document'
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    sukses: '🤗Done, Oke Desu~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
